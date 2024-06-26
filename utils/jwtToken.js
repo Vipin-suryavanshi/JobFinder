9a3f0e631fa207447b45c02ca94df0f62d65ded1
Vipin Suryavanshi <156402177+Vipin-suryavanshi@users.noreply.github.com>
2024-06-26 16:01:27 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -7,6 +7,7 @@ export const sendToken = (user, statusCode, res, message)=>{
         httpOnly: true,
          secure: true,  
         sameSite: 'None',  
+        path: '/',
     };
     res.status(statusCode).cookie("token", token,options).json({
         success: true,
