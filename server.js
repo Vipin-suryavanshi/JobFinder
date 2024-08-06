c66b0a5287118f00bfcaff224e0638d516dfc9e5
Vipin Suryavanshi <156402177+Vipin-suryavanshi@users.noreply.github.com>
2024-08-06 15:56:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
 import app from "./app.js";
-import cloudinary from "cloudinary";
 
-cloudinary.v2.config({
-    cloud_name:process.env.CLOUDINARY_CLIENT_NAME,
-    api_key:process.env.CLOUDINARY_CLIENT_API,
-    api_secret:process.env.CLOUDINARY_CLIENT_SECRET,
-    
-})
 const port = process.env.PORT || 3500
 app.listen(port, ()=>{
     console.log(`server connection establish at ${port}`);
